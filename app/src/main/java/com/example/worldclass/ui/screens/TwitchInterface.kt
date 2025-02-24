c30cb079a2580b89a5d2a79f85bcdd61b5ece92a
DiegoFrancoEscobedo <22151141@aguascalientes.tecnm.mx>
2025-02-23 20:28:44 -0600

    New top bar and add new icons in gradle

--- app/src/main/java/com/example/worldclass/ui/screens/TwitchInterface.kt
+++ app/src/main/java/com/example/worldclass/ui/screens/TwitchInterface.kt
@@ -2,8 +2,11 @@ package com.example.worldclass.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,14 +15,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -32,10 +40,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.worldclass.R
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChatBubbleOutline
+import androidx.compose.material.icons.filled.PhotoCameraFront
+
 
 @Composable
 fun TwitchInterface(navController: NavHostController) {
-    topbar()
+    topbar1()
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -44,54 +56,92 @@ fun TwitchInterface(navController: NavHostController) {
 
 
     ) {
-        Content1()
-        Content2()
+        FirstContent()
     }
 
 }
 
 @Preview(showBackground = true)
 @Composable
-fun Content1() {
-    Card(
+fun topbar1() {
+    Row(
         modifier = Modifier
-            .background(Color.LightGray)
             .fillMaxWidth()
-            .padding(5.dp)
+            .height(60.dp)
+            .background(Color.White)
+            .padding(10.dp),
+        verticalAlignment = Alignment.CenterVertically // C
+
     ) {
-
-        Text(
-            text = "This is a title",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
+        Box(
+            contentAlignment = Alignment.Center,
             modifier = Modifier
-                .padding(10.dp)
+                .size(40.dp) // Tamaño del círculo
+                .background(Color.Gray, shape = CircleShape) // Fondo circular
+        ) {
+            Icon(
+                Icons.Filled.Person,
+
+
+                contentDescription = "Person Icon",
+                tint = Color.Black,
+                modifier = Modifier
+                    .size(30.dp)
+
+            )
+
+        }
+
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Icon(
+            Icons.Filled.PhotoCameraFront,
+            contentDescription = "Photo Camera Icon",
+            tint = Color.Black,
+            modifier = Modifier
+                .size(45.dp)
+
         )
 
-        Image(
+        Spacer(modifier = Modifier.width(15.dp))
+
+        Icon(
+            Icons.Filled.MailOutline,
+            contentDescription = "Mail Icon",
+            tint = Color.Black,
             modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp),
-            painter = painterResource(R.drawable.si),
-            contentDescription = "Imagen Random",
-            contentScale = ContentScale.Crop
+                .size(45.dp)
 
         )
-        Text(
-            stringResource(R.string.sample_text2),
-            textAlign = TextAlign.Justify,
-            lineHeight = 10.sp,
+
+        Spacer(modifier = Modifier.width(15.dp))
+
+        Icon(
+            Icons.Filled.ChatBubbleOutline,
+            contentDescription = "Chat Bubble Icon",
+            tint = Color.Black,
             modifier = Modifier
-                .padding(10.dp)
+                .size(45.dp)
+
+        )
+
+        Spacer(modifier = Modifier.width(15.dp))
+
+        Icon(
+            Icons.Filled.Search,
+            contentDescription = "Search Icon",
+            tint = Color.Black,
+            modifier = Modifier
+                .size(45.dp)
+
         )
     }
-
-
 }
 
 @Preview(showBackground = true)
 @Composable
-fun Content2() {
+fun FirstContent() {
     Card(
         modifier = Modifier
             .background(Color.LightGray)
@@ -130,34 +180,4 @@ fun Content2() {
 
 
 }
-
-@Preview(showBackground = true)
-@Composable
-fun topbar1() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(60.dp)
-            .background(Color.Black)
-            .padding(10.dp)
-    ) {
-        Icon(
-            Icons.Filled.Menu,
-            contentDescription = "Menu Icon",
-            tint = Color.White,
-            modifier = Modifier
-                .size(50.dp)
-
-        )
-
-        Text(
-            stringResource(R.string.app_name),
-            color = Color.White,
-            fontSize = 30.sp
-        )
-
-    }
-
-}
-
 
