f7dd416a4a686bd78d455554827b55e21f0fb2c5
DiegoFrancoEscobedo <22151141@aguascalientes.tecnm.mx>
2025-02-28 16:38:07 -0600

    Add FirstContent

--- app/src/main/java/com/example/worldclass/ui/screens/TwitchInterface.kt
+++ app/src/main/java/com/example/worldclass/ui/screens/TwitchInterface.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -42,7 +43,11 @@ import androidx.navigation.NavHostController
 import com.example.worldclass.R
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChatBubbleOutline
+import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.PhotoCameraFront
+import androidx.compose.ui.draw.clip
+import com.example.worldclass.ui.theme.StrongPurple
 
 
 @Composable
@@ -68,7 +73,6 @@ fun topbar1() {
         modifier = Modifier
             .fillMaxWidth()
             .height(60.dp)
-            .background(Color.White)
             .padding(10.dp),
         verticalAlignment = Alignment.CenterVertically // C
 
@@ -77,14 +81,12 @@ fun topbar1() {
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .size(40.dp) // Tamaño del círculo
-                .background(Color.Gray, shape = CircleShape) // Fondo circular
+                .background(StrongPurple, shape = CircleShape) // Fondo circular
         ) {
             Icon(
                 Icons.Filled.Person,
-
-
                 contentDescription = "Person Icon",
-                tint = Color.Black,
+                tint = Color.White,
                 modifier = Modifier
                     .size(30.dp)
 
@@ -95,14 +97,27 @@ fun topbar1() {
 
         Spacer(modifier = Modifier.weight(1f))
 
-        Icon(
-            Icons.Filled.PhotoCameraFront,
-            contentDescription = "Photo Camera Icon",
-            tint = Color.Black,
-            modifier = Modifier
-                .size(45.dp)
+        Box(
+            contentAlignment = Alignment.Center,
 
-        )
+        ) {
+            Icon(
+                Icons.Filled.PhotoCameraFront,
+                contentDescription = "Photo Camera Icon",
+                tint = Color.Black,
+                modifier = Modifier
+                    .size(45.dp)
+
+            )
+            Box(
+                modifier = Modifier
+                    .size(15.dp) // Ajusta el tamaño para que cubra bien
+                    .background(Color.White)
+                    .offset(y = 30.dp)
+            )
+
+
+        }
 
         Spacer(modifier = Modifier.width(15.dp))
 
@@ -142,42 +157,310 @@ fun topbar1() {
 @Preview(showBackground = true)
 @Composable
 fun FirstContent() {
-    Card(
-        modifier = Modifier
-            .background(Color.LightGray)
-            .fillMaxWidth()
-            .padding(5.dp)
-    ) {
 
-        Row() {
-            Image(
-                modifier = Modifier
-                    .width(80.dp)
-                    .height(100.dp),
-                painter = painterResource(R.drawable.si),
-                contentDescription = "Imagen Random",
-                contentScale = ContentScale.Crop
-            )
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "Siguiendo",
+            lineHeight = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 22.sp,
+            modifier = Modifier
+                .padding(start = 10.dp, top = 10.dp, bottom = 15.dp)
+        )
+
+
+
+        Text(
+            text = "Categorías que sigues",
+            lineHeight = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 14.sp,
+            modifier = Modifier
+                .padding(start = 10.dp, bottom = 10.dp)
+        )
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp)
+        ) {
             Column() {
-                Text(
-                    text = "This is a title",
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.Bold,
+
+                Image(
                     modifier = Modifier
-                        .padding(5.dp)
+                        .width(75.dp)
+                        .height(110.dp),
+                    painter = painterResource(R.drawable.pubg),
+                    contentDescription = "Imagen de el videojuego PUBG",
+                    contentScale = ContentScale.Crop
                 )
                 Text(
-                    stringResource(R.string.sample_text2),
-                    textAlign = TextAlign.Justify,
+                    text = "PUBG: BATTLE...",
                     lineHeight = 14.sp,
-                    fontSize = 10.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    fontSize = 9.sp,
                     modifier = Modifier
-                        .padding(10.dp)
+
                 )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.Filled.Circle,
+                        contentDescription = "Circle Icon",
+                        tint = Color.Red,
+                        modifier = Modifier
+                            .size(8.dp)
+                    )
+                    Text(
+                        text = "10 K",
+                        lineHeight = 5.sp,
+                        color = Color.DarkGray,
+                        fontSize = 8.sp,
+                        modifier = Modifier
+                            .padding(horizontal = 2.dp)
+
+                    )
+                }
+
             }
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Column() {
+
+                Image(
+                    modifier = Modifier
+                        .width(75.dp)
+                        .height(110.dp),
+                    painter = painterResource(R.drawable.fortnite),
+                    contentDescription = "Imagen de el videojuego Fortnite",
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Fortnite",
+                    lineHeight = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    fontSize = 9.sp,
+                    modifier = Modifier
+
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.Filled.Circle,
+                        contentDescription = "Circle Icon",
+                        tint = Color.Red,
+                        modifier = Modifier
+                            .size(8.dp)
+
+                    )
+                    Text(
+                        text = "27.9 K",
+                        lineHeight = 5.sp,
+                        color = Color.DarkGray,
+                        fontSize = 8.sp,
+                        modifier = Modifier
+                            .padding(horizontal = 2.dp)
+
+                    )
+                }
+
+
+            }
+
+
         }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SecondContent() {
+
+    Column() {
+
+        Spacer(modifier = Modifier.height(height = 1.dp))
+
+        Text(
+            text = "Canales recomendados para ti",
+            lineHeight = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 14.sp,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp)
+        ) {
+//Imagen de la pre-vista del video
+            Box(
+                modifier = Modifier
+                    .height(110.dp)
+            ) {
+
+                Image(
+                    modifier = Modifier
+                        .width(175.dp)
+                        .height(110.dp),
+                    painter = painterResource(R.drawable.si),
+                    contentDescription = "Imagen de fondo",
+                    contentScale = ContentScale.Crop,
+
+                )
+
+
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Box(
+                        modifier = Modifier
+                            .size(14.dp)
+                            .background(Color.Red, shape = CircleShape)
+                    )
+
+
+                    Spacer(modifier = Modifier.width(4.dp))
+
+
+                    Text(
+                        text = "251",
+                        color = Color.White,
+                        fontSize = 10.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+//Descripción del directo de Twitch
+            Column() {
+
+                Row() {
+                    Image(
+                        modifier = Modifier
+                            .size(20.dp)
+                            .clip(CircleShape),
+                        painter = painterResource(R.drawable.pubg),
+                        contentDescription = "Imagen de el videojuego PUBG",
+                        contentScale = ContentScale.Crop
+                    )
+
+                    Text(
+                        text = "auron_offline",
+                        lineHeight = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        fontSize = 9.sp,
+                        modifier = Modifier
+
+                    )
+                }
+
+                Text(
+                    text = "Auronplay Juega Among U...",
+                    lineHeight = 5.sp,
+                    color = Color.DarkGray,
+                    fontSize = 8.sp,
+                    modifier = Modifier
+                        .padding(horizontal = 2.dp)
+
+                )
+
+                Text(
+                    text = "Just Chatting",
+                    lineHeight = 5.sp,
+                    color = Color.DarkGray,
+                    fontSize = 8.sp,
+                    modifier = Modifier
+                        .padding(horizontal = 2.dp)
+
+                )
+
+
+            }
+
+            Icon(
+                Icons.Filled.MoreVert,
+                contentDescription = "MoreVert Icon",
+                tint = Color.Black,
+                modifier = Modifier
+                    .size(8.dp)
+
+            )
+
+            }
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Column() {
+
+                Image(
+                    modifier = Modifier
+                        .width(75.dp)
+                        .height(110.dp),
+                    painter = painterResource(R.drawable.fortnite),
+                    contentDescription = "Imagen de el videojuego Fortnite",
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Fortnite",
+                    lineHeight = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    fontSize = 9.sp,
+                    modifier = Modifier
+
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.Filled.Circle,
+                        contentDescription = "Circle Icon",
+                        tint = Color.Red,
+                        modifier = Modifier
+                            .size(8.dp)
+
+                    )
+                    Text(
+                        text = "27.9 K",
+                        lineHeight = 5.sp,
+                        color = Color.DarkGray,
+                        fontSize = 8.sp,
+                        modifier = Modifier
+                            .padding(horizontal = 2.dp)
+
+                    )
+                }
+
+
+            }
+
+
+        }
+
     }
 
 
-}
+
+
 
